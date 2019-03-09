5fa064f699a0dfa9ef761442e2f95d749aa84224
Nguyen Trung Duc <ngtrungducyb@gmail.com>
2019-03-09 12:15:05 +0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Trung Duc on 3/9/19.
+//  Copyright © 2019 Trung Duc. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
